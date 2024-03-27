97f68e000c6c0c609b3471c44dc53c2cdad8dbb7
Ville Lautanala <lautis@gmail.com>
2024-03-27 10:41:02 +0200

    Remove declaration files from dist

--- dist/src/summary.d.ts
+++
@@ -1,5 +0,0 @@
-import { default as libCoverage } from 'istanbul-lib-coverage';
-export declare function generateSummary(file: string): Promise<libCoverage.CoverageSummary>;
-export declare function loadLCOV(file: string): Promise<libCoverage.CoverageSummary>;
-export declare function loadCobertura(file: string): Promise<libCoverage.CoverageSummary>;
-export declare function loadSummary(file: string): Promise<libCoverage.CoverageSummary>;
